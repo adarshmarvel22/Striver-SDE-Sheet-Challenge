00f912076835fc02025d63e9d41bceab3a048647
Adarsh <87609950+adarshmarvel22@users.noreply.github.com>
2022-06-08 17:34:50 +0530

    4.2b

--- Day4/4Sum_b.cpp
+++ Day4/4Sum_b.cpp
@@ -24,56 +24,34 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& num, int target) {
-      vector<vector<int> > res;
-        
-        if (num.empty())
-            return res;
-        int n = num.size(); 
-        sort(num.begin(),num.end());
-    
-        for (int i = 0; i < n; i++) {
-        
-            int target_3 = target - num[i];
-        
-            for (int j = i + 1; j < n; j++) {
-            
-                int target_2 = target_3 - num[j];
-            
-                int front = j + 1;
-                int back = n - 1;
-            
-                while(front < back) {
-                
-                    int two_sum = num[front] + num[back];
-                
-                    if (two_sum < target_2) front++;
-                
-                    else if (two_sum > target_2) back--;
-                
-                    else {
-                    
-                        vector<int> quadruplet(4, 0);
-                        quadruplet[0] = num[i];
-                        quadruplet[1] = num[j];
-                        quadruplet[2] = num[front];
-                        quadruplet[3] = num[back];
-                        res.push_back(quadruplet);
-  
-                        while (front < back && num[front] == quadruplet[2]) ++front;
-     
-                        while (front < back && num[back] == quadruplet[3]) --back;
-                
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+       int n=nums.size();
+        sort(nums.begin(),nums.end());
+        // vector<vector<int>>res;
+        set<vector<int>>ansx;
+        rep(i,0,n){
+            rep(j,i+1,n){
+                ll twosum1=target-(nums[i]+nums[j]);
+                int l=j+1;
+                int r=n-1;
+                while(l<r){
+                    if((nums[l]+nums[r])<twosum1) l++;
+                    else if(((nums[l]+nums[r])>twosum1))   r--;
+                    else{
+                        vector<int>ans(4,0);
+                        ans[0]=(nums[i]);
+                         ans[1]=(nums[j]);
+                         ans[2]=(nums[l]);
+                         ans[3]=(nums[r]);
+                        // res.pb(ans);
+                        ansx.insert(ans);
+                        while(l<r&&nums[l]==ans[2]) ++l;
+                        while(l<r&&nums[r]==ans[3])  --r;
                     }
                 }
-
-                while(j + 1 < n && num[j + 1] == num[j]) ++j;
             }
-
-            while (i + 1 < n && num[i + 1] == num[i]) ++i;
-        
         }
-    
+        vector<vector<int>>res(ansx.begin(),ansx.end());
         return res;
         
     }
