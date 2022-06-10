f81da7b53604f229260876123ae49031b3fc8e3e
Adarsh <87609950+adarshmarvel22@users.noreply.github.com>
2022-06-10 23:17:06 +0530

    6.1c

---
+++ Day6/IntersectionofListsc.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int getDifference(ListNode* head1,ListNode* head2) {
+     int len1 = 0,len2 = 0;
+        while(head1 != NULL || head2 != NULL) {
+            if(head1 != NULL) {
+                ++len1; head1 = head1->next;
+            }
+            if(head2 != NULL) {
+                ++len2; head2 = head2->next;
+            }
+            
+        }
+        return len1-len2;
+}
+    ListNode *getIntersectionNode(ListNode *head1, ListNode *head2) {
+       int diff = getDifference(head1,head2);
+        if(diff < 0) 
+            while(diff++ != 0) head2 = head2->next; 
+        else while(diff-- != 0) head1 = head1->next;
+        while(head1 != NULL) {
+            if(head1 == head2) return head1;
+            head2 = head2->next;
+            head1 = head1->next;
+        }
+        return head1;
+    }
+};
