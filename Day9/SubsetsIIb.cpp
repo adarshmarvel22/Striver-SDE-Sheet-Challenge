bee7a0288b84d450981780808d442231f43590eb
Adarsh <87609950+adarshmarvel22@users.noreply.github.com>
2022-06-16 21:43:49 +0530

    9.2b

---
+++ Day9/SubsetsIIb.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+   void fun(vector < int > & nums, int index, vector < int > ds, set < vector < int >> & res) {
+      if (index == nums.size()) {
+        sort(ds.begin(), ds.end());
+        res.insert(ds);
+        return;
+      }
+      ds.push_back(nums[index]);
+      fun(nums, index + 1, ds, res);
+      ds.pop_back();
+      fun(nums, index + 1, ds, res);
+    }
+    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector < vector < int >> ans;
+    set < vector < int >> res;
+    vector < int > ds;
+    fun(nums, 0, ds, res);
+    for (auto it = res.begin(); it != res.end(); it++) {
+      ans.push_back( * it);
+    }
+    return ans;
+    }
+};
