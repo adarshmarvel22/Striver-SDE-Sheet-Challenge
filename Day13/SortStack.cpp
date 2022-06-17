8107c944c523fbc7fd7c66d6a1b4428bde6e1139
Adarsh <87609950+adarshmarvel22@users.noreply.github.com>
2022-06-17 10:19:57 +0530

    13.7

---
+++ Day13/SortStack.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rpt(i,a,b) for(int i=a;i<b;i+=2)
+#define rev(i,a,b) for(int i=a;i>=b;i--)
+#define vi vector<int>
+# define ll long long int
+#define inf 1e9
+#define v2d vector<vector<int>>
+#define v1d vector<int>
+# define vll vector<long long int>
+# define vvi vector<pair<int,int>>
+# define vc vector<pair<char,char>>
+#define m_p make_pair
+#define p_b(x) push_back(x)
+#define ff first
+#define be(x) begin(x) end(x)
+#define rbe(x) rbegin(x) rend(x)
+#define ss second
+#define all1(x) x.begin(),x.end()
+void sortStack(stack<int> &st)
+{
+    v1d ans;
+	// Write your code here
+    while(!st.empty()){
+        ans.p_b(st.top());
+        st.pop();
+    }
+    sort(all1(ans));
+    for(auto i: ans)  st.push(i);
+}
