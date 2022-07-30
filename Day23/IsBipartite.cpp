557d5424ad9951e86f2bbbb1aefec6160d70f788
Adarsh <87609950+adarshmarvel22@users.noreply.github.com>
2022-07-30 20:40:11 +0530

    10

---
+++ Day23/IsBipartite.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+    
+        int n=graph.size();
+        // int color[n];
+    // memset(color, -1, sizeof(color));
+        vector<int>color(n,-1);
+    for(int i = 0;i<n;i++) {
+        if(color[i] == -1) {
+            color[i] = 1;
+            if(!color_dfs(i, graph, color)) {
+                return false;
+            }
+        } 
+    }
+    return true; 
+    }
+    
+    bool color_dfs(int node,vector<vector<int>>&adj, vector<int>&color) {
+        
+        // if(color[node]==-1)
+        // color[node]=1;
+    for(auto it : adj[node]) {
+        if(color[it] == -1) {
+            color[it] = 1 - color[node];
+            if(!color_dfs(it, adj, color)) {
+                return false; 
+            }
+        } else if(color[it] == color[node]) return false; 
+    }
+    return true; 
+}
+};
