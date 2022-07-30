652bf27ff6b42016101433eaa4135d7a1a7e6f54
Adarsh <87609950+adarshmarvel22@users.noreply.github.com>
2022-07-30 13:02:49 +0530

    1 a

---
+++ Day16/Implement _strStr().cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        
+        if(needle.size()==0)   return 0;
+
+       size_t idx= haystack.find(needle);
+        
+        if (idx != string::npos)
+        // if(idx!=haystack.end()) 
+            return idx;
+        
+        return -1;
+    }
+};
