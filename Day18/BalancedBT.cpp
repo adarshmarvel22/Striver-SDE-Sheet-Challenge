a8bbf6dac43b4991abcaa381b4041a68e856de80
Adarsh <87609950+adarshmarvel22@users.noreply.github.com>
2022-07-30 16:38:42 +0530

    4

---
+++ Day18/BalancedBT.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+     int ht (TreeNode *root) {
+        if (!root) return 0;
+        
+        int l = ht (root -> left);
+        if (l == -1) return -1;
+        
+        int r = ht (root -> right);
+        if (r == -1) return -1;
+        
+        if (abs(l - r) > 1)   return -1;
+        
+        return max (l, r) + 1;
+    }
+    bool isBalanced(TreeNode* root) {
+        return ht (root) != -1;
+    }
+   
+};
